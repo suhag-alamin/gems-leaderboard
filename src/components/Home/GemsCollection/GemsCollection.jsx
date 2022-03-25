55a556ecd33f50086ec3a264ead663deda812d00
Suhag Al Amin <developersuhag@gmail.com>
2022-03-25 19:38:01 +0600

    gems collection

---
+++ src/components/Home/GemsCollection/GemsCollection.jsx
@@ -0,0 +1,36 @@
+import { Container, Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import gems1 from '../../../images/GEMS01.svg'
+import gems2 from '../../../images/GEMS02.svg'
+import gems3 from '../../../images/GEMS03.svg'
+import gems4 from '../../../images/GEMS04.svg'
+
+
+
+const GemsCollection = () => {
+    return (
+        <div>
+            <Container sx={{py:8}}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid sx={{  textAlign: 'center'}} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+                        <Grid item xs={2} sm={4} md={3} >
+                            <img src={gems1} alt="" />
+                        </Grid>
+                        <Grid sx={{marginTop: {xs: 0, md: -7}}} item xs={2} sm={4} md={3} >
+                            <img src={gems2} alt="" />
+                        </Grid>
+                        <Grid item xs={2} sm={4} md={3} >
+                            <img src={gems3} alt="" />
+                        </Grid>
+                        <Grid sx={{marginTop: {xs: 0, md: -7}}} item xs={2} sm={4} md={3} >
+                            <img src={gems4} alt="" />
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </div>
+    );
+};
+
+export default GemsCollection;
--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BeGemsStar from '../BeGemsStar/BeGemsStar';
 import Event from '../Event/Event';
+import GemsCollection from '../GemsCollection/GemsCollection';
 import HeroSection from '../HeroSection/HeroSection';
 import '../Home.css';
 
@@ -10,6 +11,7 @@ const Home = () => {
             <HeroSection />
             <BeGemsStar />
             <Event />
+            <GemsCollection />
         </div>
     );
 };
